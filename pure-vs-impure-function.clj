8aa33ac19a3302980c084f768bdf7bf8eb1f7ab1
Mustafa Basit <mailtobasit74@gmail.com>
2020-03-05 23:43:21 +0530

    Add files via upload

---
+++ pure-vs-impure-function.clj
@@ -0,0 +1,85 @@
+;pure function 
+;It's return value is the same for the same arguments
+
+;This function only relies on variable explicitly passed to it, and does not mutate any 
+;external state or the argument passed to it
+
+(defn calculate-area
+  [radius]
+  (* 3.14 (* radius radius)) )
+
+(calculate-area 215)
+(calculate-area 215)
+(calculate-area 215)
+
+
+;impure function 
+(defn foo 
+  [num]
+  (+ num (rand-int 50))) 
+
+(foo 3) 
+(foo 3) 
+(foo 3) 
+
+
+(rand-int 10)
+(rand-int 10)
+(rand-int 10)
+
+
+;pure fucntion 
+;don't modify argument being passed to them
+;It's evaluation has no side effects
+;never alter state/ does not modify external variable
+
+
+;pure function 
+;no use of global variables 
+
+(defn pure-foo 
+  [x y]
+  (def mult 2)
+  (* (+ x y) mult))
+
+(pure-foo 4 5)
+
+;impure function 
+(def mult 2)
+
+(defn impure-foo
+  [x y]
+  (* (+ x y) mult))
+
+(impure-foo 4 5)
+
+;impure fucntion 
+(def x 100)
+
+(defn impure-foo
+  []
+  x)
+
+(impure-foo )
+
+;pure function 
+;takes in a specific argument and returns a determinstic value
+
+(defn increment-numbers
+  [number-collection]
+  (map inc number-collection) )
+
+(increment-numbers '(1 2 3 4 5))
+
+
+;impure function 
+;uses global value rather than the argument so the result is inderminstic
+
+(def global-value '(44 93 11 89 10) )
+
+(defn impure-increment-numbers
+  [number-collection]
+  (map inc global-value) )
+  
+(impure-increment-numbers '(1 2 3 4 5))
+
---
+++ recursion.clj
@@ -0,0 +1,48 @@
+(defn recursively-use-a-collection
+  [collection]
+  (println (first collection) )
+  
+  (if (empty? collection) 
+    (print-str "No more values to process")
+    (recursively-use-a-collection (rest collection)) ))
+
+
+(recursively-use-a-collection [1 2 3 4 5] )
+
+
+
+
+;;recursion with polymorphism
+
+(defn sum 
+  ([vals] (sum vals 0) )
+
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     (sum (rest vals) (+ (first vals) accumulating-total)))))
+   
+(sum [1 2 3 4 5] )
+(sum (vec (range 1 6)) )
+(sum [1 2 3 4 5] 100 )
+
+;if we have a very large collection, we run the risk of blowing our heap space
+;(sum (vec (range 1 999999999)) )
+
+
+;using tail call optimizaiton allows us to reuse a memory location when we call a function recursively
+
+;recur allows the processing of a very large data set without blowing the heap space
+
+
+(defn sum 
+  ([vals] (sum vals 0) )
+
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     (recur (rest vals) (+ (first vals) accumulating-total)))))
+   
+(sum [1 2 3 4 5] )
+(sum (vec (range 1 6666666)) )
+(sum [1 2 3 4 5] 100 )
