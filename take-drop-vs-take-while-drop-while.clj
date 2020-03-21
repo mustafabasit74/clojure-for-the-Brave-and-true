38109b2b17132e4316ae0039faa5474fb35eeedb
Mustafa Basit <mailtobasit74@gmail.com>
2020-03-21 19:19:41 +0530

    take drop while

---
+++ take-drop-vs-take-while-drop-while.clj
@@ -0,0 +1,41 @@
+(take 3 [1 2 3 4 5])
+(take 3 '(1 2 3 4 5))
+
+(drop 3 [1 2 3 4 5])
+(drop 3 '(1 2 3 4 5))
+
+(take-while #(< % 4) [1 2 3 4 5])
+(drop-while #(< % 4) [1 2 3 4 5])
+
+(take-while neg? [-1 -3 -93 -10 0 -3 -2 -40 10 22 49])
+; => (-1 -3 -93 -10)
+
+(drop-while neg? [-1 -3 -93 -10 0 -3 -2 -40 10 22 49])
+; => (0 -3 -2 -40 10 22 49)
+
+(def food-journal
+  [{:month 1 :day 1 :human 2 :critter 0}
+   {:month 1 :day 2 :human 5 :critter 1}
+   {:month 2 :day 1 :human 1 :critter 4}
+   {:month 2 :day 2 :human 3 :critter 2}
+   {:month 3 :day 1 :human 2 :critter 8}
+   {:month 3 :day 2 :human 1 :critter 2}
+   {:month 4 :day 1 :human 2 :critter 4}
+   {:month 4 :day 2 :human 0 :critter 5}
+   {:month 5 :day 1 :human 0 :critter 4}
+   {:month 5 :day 2 :human 2 :critter 4}]) 
+
+
+(take-while #(< (:month %) 3 ) food-journal)  
+
+(drop-while #(< (:month %) 3)  food-journal)   
+
+;get data from 3rd and 4th month
+(take-while #(< (:month %) 5 )
+            (drop-while #(< (:month %) 3) food-journal) )
+;or
+
+(drop-while #(< (:month %) 3)
+             (take-while #(< (:month %) 5) food-journal))
+
+
